94da46ec8138461671e69c4ac10bebd60b1e4d70
jhndouglass <jhndouglass@gmail.com>
2014-09-26 16:34:17 +0100

    changes to plot functions, cleaned arguments, made pretty

--- R/LAMnlsplot.R
+++ R/LAMnlsplot.R
@@ -8,7 +8,6 @@
 #'      not used
 #'
 #' @param x a "LAMnls" object
-#' @param y is ignored
 #' @param ... further arguments passed to and from other methods
 #'
 #' @examples
@@ -21,14 +20,14 @@
 #'      LAMnlsplot(LAMwensumnls)
 #'
 #' @export
-LAMnlsplot <- function(x, y, ...){
+LAMnlsplot <- function(x, ...){
         foo <- data.frame(Fpred = fitted(x), x$diffuse.point, x$raw)
         foo <- foo[order(foo$Q), ]
-        linewidth <- 1
-        pointsize <- 1.5
+        linewidth <- 0.8
+        pointsize <- 1
         titlesize <- 1
         pointalpha <- 0.5
-        axissize <- 0.9
+        axissize <- 0.8
         plottext <- 3
         # linear regression
         gg <- ggplot2::ggplot(data = foo, ggplot2::aes(Q, Co)) + ggplot2::stat_smooth(method = lm,
@@ -37,9 +36,9 @@ LAMnlsplot <- function(x, y, ...){
                                                            size = ggplot2::rel(linewidth), alpha = 0.3)
         gg <- gg + ggplot2::geom_point(size = ggplot2::rel(pointsize), alpha = pointalpha)
         gg <- gg + ggplot2::labs(title = "Linear model", x = "Q", y = "Conc.")
-        gg <- gg + ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(titlesize), face = "bold"),
-                         axis.title.x = ggplot2::element_text(face = "bold", size = ggplot2::rel(axissize)),
-                         axis.title.y = ggplot2::element_text(face = "bold", size = ggplot2::rel(axissize)),
+        gg <- gg + ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(titlesize)),
+                         axis.title.x = ggplot2::element_text(size = ggplot2::rel(axissize)),
+                         axis.title.y = ggplot2::element_text(size = ggplot2::rel(axissize)),
                          panel.background = ggplot2::element_rect(fill = NA),
                          panel.grid.minor = ggplot2::element_line(colour = NA),
                          axis.line = ggplot2::element_line(colour = "black"))
@@ -50,9 +49,9 @@ LAMnlsplot <- function(x, y, ...){
         gg2 <- gg2 + ggplot2::geom_point(size = ggplot2::rel(pointsize),
                                 alpha = pointalpha)
         gg2 <- gg2 + ggplot2::labs(title = "LAM", x = "Q", y = "Conc.")
-        gg2 <- gg2 + ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(titlesize), face = "bold"),
-                           axis.title.x = ggplot2::element_text(face = "bold", size = ggplot2::rel(axissize)),
-                           axis.title.y = ggplot2::element_text(face = "bold", size = ggplot2::rel(axissize)),
+        gg2 <- gg2 + ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(titlesize)),
+                           axis.title.x = ggplot2::element_text(size = ggplot2::rel(axissize)),
+                           axis.title.y = ggplot2::element_text(size = ggplot2::rel(axissize)),
                            panel.background = ggplot2::element_rect(fill = NA),
                            panel.grid.minor = ggplot2::element_line(colour = NA),
                            axis.line = ggplot2::element_line(colour = "black"))
@@ -65,9 +64,9 @@ LAMnlsplot <- function(x, y, ...){
         gg3 <- gg3 + ggplot2::geom_point(size = ggplot2::rel(pointsize),
                                 alpha = pointalpha)
         gg3 <- gg3 + ggplot2::labs(title = "LAM - Components", x = "Q", y = "Conc.")
-        gg3 <- gg3 + ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(titlesize), face = "bold"),
-                           axis.title.x = ggplot2::element_text(face = "bold", size = ggplot2::rel(axissize)),
-                           axis.title.y = ggplot2::element_text(face = "bold", size = ggplot2::rel(axissize)),
+        gg3 <- gg3 + ggplot2::theme(plot.title = ggplot2::element_text(size = ggplot2::rel(titlesize)),
+                           axis.title.x = ggplot2::element_text(size = ggplot2::rel(axissize)),
+                           axis.title.y = ggplot2::element_text(size = ggplot2::rel(axissize)),
                            panel.background = ggplot2::element_rect(fill = NA),
                            panel.grid.minor = ggplot2::element_line(colour = NA),
                            axis.line = ggplot2::element_line(colour = "black"))
@@ -77,21 +76,26 @@ LAMnlsplot <- function(x, y, ...){
         gg4 <- ggplot2::ggplot()
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 1.1,
                               label = paste("Linear model SS = ",
-                                            round(sum(lm1$residuals ^ 2), 2)))
+                                            round(sum(lm1$residuals ^ 2), 2)),
+                              size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 1,
-                              label = paste("LAM SS = ", round(summary(x)$SumofSq, 2)))
+                              label = paste("LAM SS = ",
+                                            round(summary(x)$SumofSq, 2)),
+                              size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 0.9,
                               label = paste("Q xover point = ", round(x$xover, 2),
-                                            " flow units"))
+                                            " flow units"),
+                              size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 0.8,
                               label = paste("% samples PS dominated = ",
-                                            round(x$ps.time, 2)))
+                                            round(x$ps.time, 2)),
+                              size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 0.7, label = "LAM fit",
-                              col = "seagreen", face = "bold")
+                              col = "seagreen", size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 0.6, label = "LAM diffuse fit",
-                              col = "steelblue3", face = "bold")
+                              col = "steelblue3", size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::annotate("text", x = 0.3, y = 0.5, label = "LAM point fit",
-                              col = "firebrick4", face = "bold")
+                              col = "firebrick4", size = ggplot2::rel(plottext))
         gg4 <- gg4 + ggplot2::theme(plot.title = ggplot2::element_blank(),
                            axis.title.x = ggplot2::element_blank(),
                            axis.title.y = ggplot2::element_blank(),
